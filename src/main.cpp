bcba29b223b2d2ebfefdcdb6bc9531c93fbe6eee
Joachim LF <joachimlf@pm.me>
2020-08-04 11:25:39 +0200

    Added sysexits

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-/* #ifdef HAVE_SYSEXITS_H
+#ifdef HAVE_SYSEXITS_H
 # include <sysexits.h>
 #else
 # define EX_OK       0
 # define EX_USAGE	 64
 # define EX_IOERR	 74
-#endif */
+#endif
 
 int main(int argc, char **argv) {
   std::cout << "Epsilon DFU" << std::endl;
-  return 0;
+  exit(EX_OK);
 }
